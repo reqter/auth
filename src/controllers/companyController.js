19bbf7efcdcfd0b805ffa948b2ab71ac39b1c18e
fakhrad <fakhrad@openratio.com>
2019-07-15 21:01:05 +0430

    console log added

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -118,7 +118,7 @@ var findById = function(req, cb)
 //
 var addcompany = function(req, cb)
 {
-    console.log("add company controller : " + req.body);
+    console.log("add company controller : " + JSON.stringify(req.body));
     var company = new Companies();
     company = Object.assign(company, req.body);
     company.sys.type = "company";
